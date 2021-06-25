cbccdfc7e5c7d95fba98e4e53dd5c21f0690f1bc
Kevin Le <kevinle108@gmail.com>
2021-06-25 12:53:00 -0400

    refactored task list and tile

--- lib/screens/tasks_screens.dart
+++ lib/screens/tasks_screens.dart
@@ -1,45 +1,101 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:todoey_flutter/widgets/task_list.dart';
 
 class TaskScreen extends StatelessWidget {
-  const TaskScreen({Key? key}) : super(key: key);
+
+  List<String> tasks = [
+    'Mow grass',
+    'Water plants',
+    'Fix car',
+    'Babysit',
+    'Go shopping',
+  ];
+
+  List<Widget> toDoList() {
+    List<Widget> taskWidgets = [];
+    for (String task in tasks) {
+      Widget taskWidget = Text(
+          task,
+          style: TextStyle(
+            fontSize: 80.0,
+          ),
+      );
+      taskWidgets.add(taskWidget);
+    }
+    return taskWidgets;
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.lightBlueAccent,
-      body: Container(
-        padding: EdgeInsets.only(
-          top: 60.0,
-          left: 30.0,
-          right: 30.0,
-          bottom: 30.0,
-        ),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            CircleAvatar(
-              child: Icon(
-                Icons.list,
-                size: 30.0,
-                color: Colors.lightBlueAccent,
-              ),
-              backgroundColor: Colors.white,
-              radius: 30.0,
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        backgroundColor: Colors.lightBlueAccent,
+        child: Icon(Icons.add),
+      ),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Container(
+            padding: EdgeInsets.only(
+              top: 60.0,
+              left: 30.0,
+              right: 30.0,
+              bottom: 30.0,
             ),
-            SizedBox(
-              height: 10.0,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                CircleAvatar(
+                  child: Icon(
+                    Icons.list,
+                    size: 30.0,
+                    color: Colors.lightBlueAccent,
+                  ),
+                  backgroundColor: Colors.white,
+                  radius: 30.0,
+                ),
+                SizedBox(
+                  height: 10.0,
+                ),
+                Text(
+                  'Todoey',
+                  style: TextStyle(
+                    fontSize: 50.0,
+                    color: Colors.white,
+                    fontWeight: FontWeight.w700,
+                  ),
+                ),
+                Text(
+                  '12 Tasks',
+                  style: TextStyle(
+                    fontSize: 18.0,
+                    color: Colors.white,
+                  ),
+                ),
+              ],
             ),
-            Text(
-              'Todoey',
-              style: TextStyle(
-                fontSize: 50.0,
+          ),
+          Expanded(
+            child: Container(
+              padding: EdgeInsets.only(left: 20.0),
+              decoration: BoxDecoration(
                 color: Colors.white,
-                fontWeight: FontWeight.w700,
+                borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(20.0),
+                  topRight: Radius.circular(20.0),
+                ),
               ),
+              child: TaskList(),
             ),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
 }
+
+
+
