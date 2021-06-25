bdba9306d81df891295c58ea3f74cd1b6ba2e6a6
Kevin Le <kevinle108@gmail.com>
2021-06-25 14:31:34 -0400

    local state checkbox

--- lib/widgets/task_tile.dart
+++ lib/widgets/task_tile.dart
@@ -10,10 +10,31 @@ class TaskTile extends StatelessWidget {
   Widget build(BuildContext context) {
     return ListTile(
       title: Text('This is a task.'),
-      trailing: Checkbox(
-        value: false,
-        onChanged: (value){},
-      ),
+      trailing: TaskTileCheckbox(),
     );
   }
 }
+
+class TaskTileCheckbox extends StatefulWidget {
+
+  @override
+  State<TaskTileCheckbox> createState() => _TaskTileCheckboxState();
+}
+
+class _TaskTileCheckboxState extends State<TaskTileCheckbox> {
+  bool isChecked = false;
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Checkbox(
+      value: isChecked,
+      onChanged: (newValue){
+        setState(() {
+          isChecked = newValue != null ? newValue : false;
+        });
+      },
+    );
+  }
+}
+
